a769fc4a1bd2c8679286cb98ccacbbf0a5673a01
lakmald <ldes0615@sysco.com>
2022-06-15 22:40:18 +0530

    Test class optimized

--- src/test/java/CategoriesResponseTest.java
+++ src/test/java/CategoriesResponseTest.java
@@ -1,8 +1,6 @@
 import org.testng.annotations.BeforeClass;
 import responseObjects.PromotionsDTO;
 import responseMethods.GetMethods;
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
 import org.json.simple.parser.ParseException;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -21,31 +19,6 @@ public class CategoriesResponseTest {
     }
 
     @Test
-    public void getResponse() {
-        String baseUri = "https://api.tmsandbox.co.nz/v1/Categories/@categoryId/Details.json";
-        String paramCatalogue = "?catalogue=@isCatalogue";
-        String categoryId = "6327";
-        String isCatalogue = "false";
-
-        String uri = baseUri.replace("@categoryId", categoryId) + paramCatalogue.replace("@isCatalogue", isCatalogue);
-        Response response = RestAssured.get(uri);
-
-        Assert.assertEquals(response.statusCode(), 200, "Invalid status code received");
-        Assert.assertEquals(response.getBody().path("CategoryId"), Integer.valueOf(categoryId), "Incorrect category id retrieved");
-        Assert.assertEquals(response.getBody().path("Name"), "Carbon credits");
-        Assert.assertEquals(response.getBody().path("CanRelist"), true);
-
-        List<Object> promotions = response.getBody().path("Promotions");
-        for (int i=0; i < promotions.size(); i++) {
-            String namePath = "Promotions[@i].Name";
-            String descriptionPath = "Promotions[@i].Description";
-            if (response.getBody().path(namePath.replace("@i", String.valueOf(i))).equals("Gallery")) {
-                Assert.assertEquals(response.getBody().path(descriptionPath.replace("@i", String.valueOf(i))), "Good position in category");
-            }
-        }
-    }
-
-    @Test
     public void verifyGetCategoriesResponse() throws IOException, ParseException {
         String categoryName = "Carbon credits";
         String promoName = "Gallery";
@@ -55,20 +28,8 @@ public class CategoriesResponseTest {
 
         Assert.assertTrue(response.getName().equalsIgnoreCase(categoryName), "Category Name is incorrect");
         Assert.assertTrue(response.getCanRelist(), "CanRelist value is incorrect");
-        Assert.assertTrue(isPromotionAvailable(response.getPromotions(), promoName), "Promotion is not available");
         Assert.assertTrue(getPromotionDescription(response.getPromotions(), promoName).equalsIgnoreCase(promoDescription),
                 "Promotion does not have expected Description");
-    }
-
-    private boolean isPromotionAvailable(List<PromotionsDTO> promotions, String promoName) {
-        boolean isPromotionAvailable = false;
-        if (promotions.isEmpty()) { return false; }
-        for (PromotionsDTO promotion : promotions) {
-            if (promotion.getName().equalsIgnoreCase(promoName)) {
-                isPromotionAvailable = true;
-            }
-        }
-        return isPromotionAvailable;
     }
 
     private String getPromotionDescription(List<PromotionsDTO> promotions, String promoName) {
@@ -77,6 +38,7 @@ public class CategoriesResponseTest {
         for (PromotionsDTO promotion : promotions) {
             if (promotion.getName().equalsIgnoreCase(promoName)) {
                 description = promotion.getDescription();
+                break;
             }
         }
         return description;
